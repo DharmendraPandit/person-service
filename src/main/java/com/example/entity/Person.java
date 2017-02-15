b52e272b9cb136377ba4d39c87cda8c1d1e75a58
Dharmendra Pandit <dkpandit.08@gmail.com>
2017-02-15 23:15:18 +0530

    Create Person.java

---
+++ src/main/java/com/example/entity/Person.java
@@ -0,0 +1,80 @@
+package com.example.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="PERSON")
+public class Person implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4702927963557455150L;
+
+	@Id
+	@GeneratedValue
+	@Column(name="PERSON_ID")
+	private long id;
+	
+	@Column(name="FIRST_NAME")
+	private String firstName;
+	
+	@Column(name="LAST_NAME")
+	private String lastName;
+	
+	@Column(name="EMAIL_ADDRESS")
+	private String emailAddress;
+	
+	public Person(){
+		super();
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+	
+	@Override
+	public String toString() {
+		final StringBuilder sb = new StringBuilder("Person{");
+		sb.append("id=").append(id);
+		sb.append(", firstName='").append(firstName).append('\'');
+		sb.append(", lastName='").append(lastName).append('\'');
+		sb.append(", emailAddress='").append(emailAddress).append('\'');
+		sb.append('}');
+		return sb.toString();
+	}
+}
